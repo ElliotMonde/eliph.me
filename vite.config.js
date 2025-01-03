f7c42759f19594fa8c9636a2c5e4fa2892e77467
ElliotMonde <elliotphua@gmail.com>
2025-01-03 10:59:16 +0800

    Set base for vite config.

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      _APP_ENV__: JSON.stringify(env.APP_ENV)
+    },
+    plugins: [react()],
+    base: `/${env.ASSET_URL}/`,
+    server: {
+      port: "3000"
+    }
+  }
+})
