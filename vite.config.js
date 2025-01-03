a39a12f2f8f88731a2e79f24f9b4b8f2e5932b16
ElliotMonde <elliotphua@gmail.com>
2025-01-03 19:39:25 +0800

    Changes.

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({mode}) => {
       _APP_ENV__: JSON.stringify(env.APP_ENV)
     },
     plugins: [react()],
-    base: `/${env.BASE_URL}/`,
+    base: process.env.GITHUB_ACTIONS ? `/${env.BASE_URL}/` : '/',
     server: {
       port: `${env.PORT}`
     },
