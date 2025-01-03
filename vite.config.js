a96aac36bcb6458703e1516daf51677a87cbfb4a
ElliotMonde <elliotphua@gmail.com>
2025-01-03 13:29:52 +0800

    Changes.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
@@ -12,7 +12,7 @@ export default defineConfig(({ mode}) => {
     plugins: [react()],
     base: `/${env.ASSET_URL}/`,
     server: {
-      port: "3000"
+      port: `${env.PORT}`
     }
   }
 =======
