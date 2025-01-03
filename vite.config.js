fd4c8e5da9a160be83e4b2c0a771f85e5d37afa4
ElliotMonde <elliotphua@gmail.com>
2025-01-03 21:29:14 +0800

    Changes.

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({mode}) => {
       _APP_ENV__: JSON.stringify(env.APP_ENV)
     },
     plugins: [react()],
-    base: `/eliph.me`,
+    base: `/`,
     server: {
       port: `${env.PORT}`
     },
