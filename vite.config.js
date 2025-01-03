6b60f32f08c26a921112bdc874d931017b836230
ElliotMonde <elliotphua@gmail.com>
2025-01-03 16:34:52 +0800

    Changes.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       _APP_ENV__: JSON.stringify(env.APP_ENV)
     },
     plugins: [react()],
-    base: `/${env.ASSET_URL}/`,
+    base: `/${env.BASE_URL}/` || '/',
     server: {
       port: `${env.PORT}`
     }
