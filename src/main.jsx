899d3a946efe618e7cb6936b6e81b33a99f2aefc
ElliotMonde <elliotphua@gmail.com>
2025-01-03 18:42:14 +0800

    Changes.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={"/eliph.me/"}>
+      <Routes>
+        <Route path="*" element={ <App/>} />
+      </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
