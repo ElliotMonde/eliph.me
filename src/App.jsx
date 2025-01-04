daf806c80f8d3fcd739d9c859e77dc91d688a197
ElliotMonde <elliotphua@gmail.com>
2025-01-04 23:22:21 +0800

    Add viewport size checking to autoclose burger menu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage';
-
 export default function App() {
   return (
-    <>
+    <div>
       <Header/>
       <LandingPage />
-    </>
+    </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import MenuUnderline from "./MenuUnderline";
 import { BurgerIcon } from "../assets/svgIcons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Header() {
     const [isNavClosed, setIsNavClosed] = useState(true);
     const navTitles = ["projects", "misc", "contact"];
+    useEffect(() => {
+        window.addEventListener("resize", () => { if (window.innerWidth >= 1280) setIsNavClosed(true) });
+    }, [])
+
     return (
         <div className='absolute w-full m-0 p-0 spacing'>
             <nav className="justify-end xl:flex flex-row gap-11 hidden xl:visible">
@@ -13,9 +17,9 @@ export default function Header() {
                 )}
             </nav>
             <nav className="visible xl:hidden w-full flex flex-col">
-                <button onClick={() => { setIsNavClosed(!isNavClosed) }}><BurgerIcon/></button>
-                <div className={`${window.innerWidth >= 1280 || isNavClosed ? `hidden` : ``} flex flex-col h-[90vh] bg-white`}>
-                    { navTitles.map( str => <MenuUnderline str={str} key={str} />)}
+                <button onClick={() => { setIsNavClosed(!isNavClosed) }}><BurgerIcon /></button>
+                <div className={`${isNavClosed ? `hidden` : ``} flex flex-col h-[90vh] bg-white`}>
+                    {navTitles.map(str => <MenuUnderline str={str} key={str} />)}
                 </div>
             </nav>
         </div>
