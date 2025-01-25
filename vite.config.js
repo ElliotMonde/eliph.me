c825f62bbc8c9202e41e68efe9d3c64a0b649e37
ElliotMonde <elliotphua@gmail.com>
2025-01-25 14:18:39 +0800

    Change photo url.

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,27 @@ export default defineConfig(({mode}) => {
     server: {
       port: `${env.PORT}`
     },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+
+          assetFileNames: ({ name }) => {
+            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+              return 'assets/images/[name]-[hash][extname]';
+            }
+
+            if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name]-[hash][extname]';
+            }
+
+            // default value
+            // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      }
+    }
   }
 })
